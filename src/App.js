9fa69336d980464d4aba83d7d28ba13e8fbf8b11
Lin <awsjlin@08f8bc621df8.ant.amazon.com>
2021-04-28 11:28:52 +0800

    adding Auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { Auth } from 'aws-amplify';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>We now have Auth!</h1>
+        <button onClick={callGetEntry}>Get Entry!</button>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+function callGetEntry() {
+  console.log('This is your data');
+  
+  Auth.currentSession()
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+}
+
+
+export default withAuthenticator(App);
