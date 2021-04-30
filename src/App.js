a5326212ec6fdd5fe806bbb1f5471b29b0687170
Lin <awsjlin@08f8bc621df8.ant.amazon.com>
2021-04-30 18:10:46 +0800

    Authorization

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import { Auth } from 'aws-amplify';
+import Amplify, { API, Auth } from 'aws-amplify';
+import rp from 'request-promise'
+
+const apiName = "https://29x0zqh2mi.execute-api.us-east-1.amazonaws.com/prod/"
+const pathTable = "table"
+const queryYear = "?year=2020"
+const myInit = {
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  response: true
+}
 
 function App() {
   return (
@@ -10,20 +21,39 @@ function App() {
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
-        <button onClick={callGetEntry}>Get Entry!</button>
+        <button onClick={() => getTable(pathTable)}>Get Table</button><br></br>
+        <button onClick={() => getTable(pathTable+queryYear)}>Get Film by Year 2020</button>
       </header>
       <AmplifySignOut />
     </div>
   );
 }
 
-function callGetEntry() {
-  console.log('This is your data');
+function getTable(path) {
+  console.log('Get DynamoDB table');
   
   Auth.currentSession()
-  .then(data => console.log(data))
+  .then(data => {
+    //let authToken = data.getIdToken().getJwtToken();
+    //console.log(authToken)
+    const funcName = async () => {
+      fetch(apiName + path, {
+        method: "GET"
+      }).then(function(response) {
+        console.log(response)
+        return response.text();
+      }, function(error) {
+        console.log(error.message); //=> String
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        alert(myJson);
+      });
+    };
+  
+    funcName();
+  })
   .catch(err => console.log(err));
 }
-
 
 export default withAuthenticator(App);
