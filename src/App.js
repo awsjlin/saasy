fba7ed8dcc8372b9ee15f2c9dbf73662bafaa223
Lin <awsjlin@08f8bc621df8.ant.amazon.com>
2021-05-03 15:20:10 +0800

    checking in Amazon Translate/AWS Polly buttons

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Amplify, { API, Auth } from 'aws-amplify';
 
 const apiName = "https://29x0zqh2mi.execute-api.us-east-1.amazonaws.com/prod/"
 const pathTable = "table"
+const pathTranslate = "translate"
+const pathPolly = "polly"
 const queryYear = "?year=2020"
 const myInit = {
   headers: {
@@ -15,20 +17,26 @@ const myInit = {
 }
 
 function App() {
+  const getPolly = () => {
+    //audio.play()
+  }
+
   return (
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
-        <button onClick={() => getTable(pathTable)}>Get Table</button><br></br>
-        <button onClick={() => getTable(pathTable+queryYear)}>Get Film by Year 2020</button>
+        <p><button onClick={() => getTable(pathTable)}>Get Table</button></p>
+        <p><button onClick={() => getTable(pathTable+queryYear)}>Get Film by Year 2020</button></p>
+        <p><button onClick={() => getTable(pathTranslate)}>Translate</button></p>
+        <p><button onClick={() => getTable(pathPolly, true)}>Read Movies Table</button></p>
       </header>
       <AmplifySignOut />
     </div>
   );
 }
 
-function getTable(path) {
+function getTable(path, isBinary) {
   console.log('Get DynamoDB table');
   
   Auth.currentSession()
@@ -42,17 +50,29 @@ function getTable(path) {
           Authorization: data.getIdToken().getJwtToken()
         }
       }).then(function(response) {
-        console.log(response)
-        return response.text();
+        if (isBinary)
+          return response.blob();
+        else
+          return response.text();
       }, function(error) {
         console.log(error.message); //=> String
       })
-      .then(function(myJson) {
-        console.log(myJson);
-        alert(myJson);
+      .then(function(myJson)
+      {
+        if (isBinary)
+        {
+          console.log(myJson)
+          let blob = new Blob([myJson], {type: 'audio/mp3'});
+          let url = window.URL.createObjectURL(blob)
+          window.audio = new Audio();
+          window.audio.src = url;
+          window.audio.play();
+        }
+        else
+          alert(myJson);
       });
     };
-  
+
     funcName();
   })
   .catch(err => console.log(err));
